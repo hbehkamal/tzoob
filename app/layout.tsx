cec8ff24f847b4f3a4f060cc3a28cde2082ea03a
Hosein Behkamal <hoseynbehkamal@gmail.com>
2023-07-09 19:25:30 +0330

    feat: add some sass mixin & variables and add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
-import "./globals.css";
+import "#/_styles/globals.scss";
 import { Inter } from "next/font/google";
 
 // const inter = Inter({ subsets: ["latin"] });
+
+import styles from "./page.module.scss";
 
 export const metadata = {
   title: "Tzoob",
@@ -15,7 +17,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header className={styles["home-page-header"]}>
+          <ul className={styles["home-page-header--menu"]}>
+            <li>Women</li>
+            <li>Men</li>
+            <li>Children</li>
+          </ul>
+          <div className={styles["home-page-header--logo"]}>
+            Boozt<span>.com</span>
+          </div>
+          <ul className={styles["home-page-header--menu"]}>
+            <li>Home</li>
+            <li>Beauty</li>
+            <li>Sports</li>
+          </ul>
+        </header>
+        {children}
+        <footer className={styles["home-page-footer"]}>Footer</footer>
+      </body>
     </html>
   );
 }
