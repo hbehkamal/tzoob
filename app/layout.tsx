407513efd4b09be4c6bf081b28e64f4ad7943a56
Hosein Behkamal <hoseynbehkamal@gmail.com>
2023-07-09 19:45:19 +0330

    feat: add footer content

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,30 @@ export default function RootLayout({
           </ul>
         </header>
         {children}
-        <footer className={styles["home-page-footer"]}>Footer</footer>
+        <footer className={styles["home-page-footer"]}>
+          <ul className={styles["home-page-footer--menu"]}>
+            <li>Women</li>
+            <li>Men</li>
+            <li>Children</li>
+          </ul>
+          <ul className={styles["home-page-footer--menu"]}>
+            <li>Home</li>
+            <li>Beauty</li>
+            <li>Sports</li>
+          </ul>
+          <ul className={styles["home-page-footer--menu"]}>
+            <li>Lorem</li>
+            <li>Ipsum</li>
+            <li>Dummy</li>
+            <li>Text</li>
+          </ul>
+          <ul className={styles["home-page-footer--menu"]}>
+            <li>Printing</li>
+            <li>Typesetting </li>
+            <li>Industry</li>
+            <li>Standard</li>
+          </ul>
+        </footer>
       </body>
     </html>
   );
