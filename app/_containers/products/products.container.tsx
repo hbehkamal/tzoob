3465343deafbc9d876e44bedf982c285909d8c69
Hosein Behkamal <hoseynbehkamal@gmail.com>
2023-07-10 19:04:30 +0330

    fix: update global layout styles

--- app/_containers/products/products.container.tsx
+++ app/_containers/products/products.container.tsx
@@ -7,7 +7,7 @@ import { Container } from "@chakra-ui/react";
 const ProductsContainer = () => {
   return (
     <ProductsProvider>
-      <Container maxW="1280px" centerContent bgColor="gray.50">
+      <Container maxW="1280px" centerContent bgColor="gray.50" flex="1">
         <Products />
       </Container>
     </ProductsProvider>
